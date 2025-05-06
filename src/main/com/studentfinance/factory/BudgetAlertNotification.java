93b26cd475a50028025863325495f136a21329b4
daniilrusanov <daniil12099@gmail.com>
2025-05-06 13:27:45 +0300

    small code fix

--- src/main/com/studentfinance/factory/BudgetAlertNotification.java
+++ src/main/com/studentfinance/factory/BudgetAlertNotification.java
@@ -1,9 +1,17 @@
 package main.com.studentfinance.factory;
 
 import main.com.studentfinance.model.Notification;
+import main.com.studentfinance.model.NotificationType;
 
 public class BudgetAlertNotification extends Notification {
     public BudgetAlertNotification(String message) {
-        super();
+        super(message);
+        this.type = NotificationType.BUDGET_ALERT;
+    }
+
+    @Override
+    public boolean send() {
+        System.out.println("Sending budget alert notification");
+        return super.send();
     }
 }
